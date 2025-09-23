57e702b2773faaf702cb3ad83178a36d3831938d
Pearson Kumbigi <pkumbigi@gmail.com>
2025-09-24 01:21:50 +1000

    Fix wsgi.py import for mynid_backend deployment

--- mynid_backend/wsgi.py
+++ mynid_backend/wsgi.py
@@ -6,7 +6,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app import create_app
-
+from mynid_backend import app  # Import your Flask app
 application = create_app()
 
 if __name__ == "__main__":
